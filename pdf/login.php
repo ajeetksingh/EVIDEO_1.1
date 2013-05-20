28a01474bd1c5fe55aca00f3a8e14c7eee90ef28
medialab <ajeetsingh0712@gmail.com>
2013-05-20 14:52:25 +0530

    Remove Duplicated directory

--- pdf/login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-$db_hostname = 'localhost';
-$db_database = 'matterhorn';
-$db_username = 'root';
-$db_password = '123';
-?>
--- pdf/video.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-IFS=$'\n' arr=($(find /tmp/opencast/workspace/mediapackage/$1 -name '*.mp4')); 
-echo "$arr"
